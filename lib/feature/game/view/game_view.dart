04572b826bcea044ea1b2aa1c561c719538de282
hakandl <1hakandagli@gmail.com>
2023-02-19 00:40:50 +0300

    system navigator control

--- lib/feature/game/view/game_view.dart
+++ lib/feature/game/view/game_view.dart
@@ -30,76 +30,82 @@ class GameView extends StatelessWidget {
       },
       onPageBuilder: (context, value) => ChangeNotifierProvider(
         create: (context) => value,
-        builder: (context, child) => Scaffold(
-          body: SafeArea(
-            child: Column(
-              children: [
-                context.emptySizedHeightBoxLow,
-                Expanded(
-                  flex: 2,
-                  child: Padding(
-                    padding: context.horizontalPaddingLow,
-                    child: Row(
-                      children: [
-                        Expanded(
-                          flex: 4,
-                          child: TeamScoreCard(
-                              name: context.watch<SettingsViewModel>().firstTeamTextField.text,
-                              score: context.watch<GameViewModel>().firstTeamScore.toString()),
-                        ),
-                        Expanded(
-                            flex: 2,
-                            child: TimerCard(
-                                onTap: () => context.read<GameViewModel>().stopTimer(),
-                                timer: context.watch<GameViewModel>().remainingTime)),
-                        Expanded(
+        builder: (context, child) => WillPopScope(
+          onWillPop: () async {
+            context.read<GameViewModel>().stopTimer();
+            return false;
+          },
+          child: Scaffold(
+            body: SafeArea(
+              child: Column(
+                children: [
+                  context.emptySizedHeightBoxLow,
+                  Expanded(
+                    flex: 2,
+                    child: Padding(
+                      padding: context.horizontalPaddingLow,
+                      child: Row(
+                        children: [
+                          Expanded(
                             flex: 4,
                             child: TeamScoreCard(
-                                name: context.watch<SettingsViewModel>().secondTeamTextField.text,
-                                score: context.watch<GameViewModel>().secondTeamScore.toString())),
-                      ],
+                                name: context.watch<SettingsViewModel>().firstTeamTextField.text,
+                                score: context.watch<GameViewModel>().firstTeamScore.toString()),
+                          ),
+                          Expanded(
+                              flex: 2,
+                              child: TimerCard(
+                                  onTap: () => context.read<GameViewModel>().stopTimer(),
+                                  timer: context.watch<GameViewModel>().remainingTime)),
+                          Expanded(
+                              flex: 4,
+                              child: TeamScoreCard(
+                                  name: context.watch<SettingsViewModel>().secondTeamTextField.text,
+                                  score: context.watch<GameViewModel>().secondTeamScore.toString())),
+                        ],
+                      ),
                     ),
                   ),
-                ),
-                context.emptySizedHeightBoxLow,
-                Expanded(
-                  flex: 11,
-                  child: PageView.builder(
-                      controller: value.pageController,
-                      physics: const NeverScrollableScrollPhysics(),
-                      itemCount: value.wordsList.length,
-                      itemBuilder: (context, index) {
-                        if (context.watch<GameViewModel>().timer != null &&
-                            !context.watch<GameViewModel>().timer!.isActive) {
-                          if (value.remainingTime == 0 &&
-                                  value.firstTeamScore >= context.read<SettingsViewModel>().score ||
-                              value.secondTeamScore >= context.read<SettingsViewModel>().score) {
-                            return GameStatusCard(
-                              icon: Icons.thumb_up_outlined,
-                              text:
-                                  'Oyun bitti, kazanan ${value.firstTeamScore > value.secondTeamScore ? context.watch<SettingsViewModel>().firstTeamTextField.text : context.watch<SettingsViewModel>().secondTeamTextField.text} oldu',
-                            );
-                          } else if (context.watch<GameViewModel>().remainingTime == 0) {
-                            return const GameStatusCard(
-                              icon: Icons.timer_outlined,
-                              title: 'Süre doldu',
-                              text: 'Telefonu diğer takıma verin',
-                            );
-                          } else {
-                            return const GameStatusCard(icon: Icons.pause);
+                  context.emptySizedHeightBoxLow,
+                  Expanded(
+                    flex: 11,
+                    child: PageView.builder(
+                        controller: value.pageController,
+                        physics: const NeverScrollableScrollPhysics(),
+                        itemCount: value.wordsList.length,
+                        itemBuilder: (context, index) {
+                          if (context.watch<GameViewModel>().timer != null &&
+                              !context.watch<GameViewModel>().timer!.isActive) {
+                            if (value.remainingTime == 0 &&
+                                    value.firstTeamScore >= context.read<SettingsViewModel>().score ||
+                                value.secondTeamScore >= context.read<SettingsViewModel>().score) {
+                              return GameStatusCard(
+                                icon: Icons.thumb_up_outlined,
+                                text:
+                                    'Oyun bitti, kazanan ${value.firstTeamScore > value.secondTeamScore ? context.watch<SettingsViewModel>().firstTeamTextField.text : context.watch<SettingsViewModel>().secondTeamTextField.text} oldu',
+                              );
+                            } else if (context.watch<GameViewModel>().remainingTime == 0) {
+                              return const GameStatusCard(
+                                icon: Icons.timer_outlined,
+                                title: 'Süre doldu',
+                                text: 'Telefonu diğer takıma verin',
+                              );
+                            } else {
+                              return const GameStatusCard(icon: Icons.pause);
+                            }
                           }
-                        }
-                        return TabooCard(
-                            word: value.wordsList[index].word ?? '', taboo: value.wordsList[index].taboo ?? '');
-                      }),
-                ),
-                Expanded(
-                  flex: 2,
-                  child: BottomButtons(
-                    value: value,
+                          return TabooCard(
+                              word: value.wordsList[index].word ?? '', taboo: value.wordsList[index].taboo ?? '');
+                        }),
                   ),
-                ),
-              ],
+                  Expanded(
+                    flex: 2,
+                    child: BottomButtons(
+                      value: value,
+                    ),
+                  ),
+                ],
+              ),
             ),
           ),
         ),
