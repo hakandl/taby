30cd7574a630fe57c8c1e6da566c0805a7d7be1c
hakandl <1hakandagli@gmail.com>
2023-02-19 22:07:33 +0300

    app title edit

--- lib/main.dart
+++ lib/main.dart
@@ -27,7 +27,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Material App',
+      title: 'taby',
       home: const HomeView(),
       theme: AppThemeLight.instance.theme,
     );
